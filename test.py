d269eda26b8219d863757c29542a9e9fb7729568
horii <horii@adminnoMacBook-Pro.local>
2017-11-15 19:00:01 +0900

    testcoment

--- test.py
+++ test.py
@@ -1,1 +1,17 @@
 # Pythonhorii2
+#そのままだとssl認証エラーが発生するため追記
+import ssl
+ssl._create_default_https_context = ssl._create_unverified_context
+#
+
+import urllib.request
+from bs4 import BeautifulSoup
+req = urllib.request.urlopen("http://quality-start.in/company/")
+soup = BeautifulSoup(req,"html.parser")
+divtab = soup.find("div",class_="list-group")
+array = []
+array = divtab.find_all("a",class_="list-group-item")
+
+for v in array:
+    print(v.string)
+#testだよ
